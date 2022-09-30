159b18682894acb3c9c273a53079542abce14df6
Weicong Kong <weicong.kong@gmail.com>
2022-09-30 21:13:16 +1000

    add the logic to determine the start of the week given the year and week number

--- demo-2022-09-30.py
+++ demo-2022-09-30.py
@@ -5,8 +5,24 @@ Copyright (C) Weicong Kong, 30/09/2022
 """
 
 import glob
+import pandas as pd
 
 exts = ['md', 'csv', 'pdf']
 for ext in exts:
 	files = glob.glob(f'./**/*.{ext}', recursive=True)
 	print(files)
+
+
+# WK: read_excel solves a lot of excel reading problem
+excel_df = pd.read_excel()
+
+
+# WK: num of week in a year
+
+import datetime
+
+week_num = 38
+year = 2022
+weekday = 0  # Sunday
+week_start_at = datetime.datetime.strptime(f'{year}-{week_num}-{weekday}', "%Y-%W-%w")
+print(week_start_at)
